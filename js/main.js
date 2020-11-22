61e69c9d9393b2ddd79446762511ebe457d3aece
ipolit161 <ipolit161@gmail.com>
2020-11-22 22:04:14 +0300

    added slick slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 $(function(){
+
     $('.questions__item').on('click', function(){
         $('.questions__item').removeClass('questions__item--active');
         $(this).toggleClass('questions__item--active');
@@ -8,23 +9,50 @@ $(function(){
     $('.calculator__select-button').on('click', function(){
         $('.calculator__select-button').toggleClass('calculator__select-button--active');
     });
+
+    $('.nav__btn').on('click', function(){
+        $('.nav__btn').toggleClass('nav__btn--active');
+        $('.header__items').toggleClass('header__items--active');
+    });
 });
 
-let sumLising = document.getElementById('sum-lising')
+
+let sumLising = document.getElementById('sum-lising');
 let slider = document.getElementById('rangeSlider');
 let output = document.getElementById('value');
+let inputValueMax = document.querySelector('input__advance-max');
+
+function buttonUp() {
+    sumLising.addEventListener('keydown', function(e){
+        if(e.keyCode === 13){
+            // to get input
+        let getInput = document.getElementById('sum-lising').value;
+        let changeInput = slider.setAttribute('max', getInput);
+       // let addText = getInput + ' руб';
+       // inputValueMax.innerHTML = addText;
+    }
+});
+};
+buttonUp()
+
+
+
 
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
-    output.placeholder = this.value;
 
+slider.oninput = function() {
+    output.value = this.value;
+   
     slider.addEventListener('mousemove', function() {
         var x = slider.value;
         var color = 'linear-gradient(90deg, rgb(58,118,240)'+ x + '%, rgb(230,236,244)' + x +'%)';
         slider.style.background = color;
     });
 };
+
+
+
 
 slider.rangeslider({
       polyfill: false
@@ -35,3 +63,4 @@ slider.rangeslider({
 output.on('input', function(){
     slider.val(this.value).change();
 });
+
